e000aae2be6bfd55dc748248d0ba8465a047e4ba
unknown <nuriswanto66@gmail.com>
2022-10-17 09:06:11 -0700

    Tambahkan routes ke server.js

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@
 module.exports = (app) => {
     var jsonku = require('./controller');
 
+    // jsonku adalah controller dan didalam controller ada menu index
     app.route('/')
         .get(jsonku.index)
 }
