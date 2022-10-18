553bae6bfa67d72adc96109abaeb994b94c588f2
unknown <nuriswanto66@gmail.com>
2022-10-18 03:09:48 -0700

    Membuat register

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,38 +2,38 @@ const mysql = require('mysql');
 const connection = require('../koneksi');
 const md5 = require('MD5');
 const response = require('../res');
-const jwt = require('jasonwebtoken');
+const jwt = require('jsonwebtoken');
 const config = require('../config/secret');
 const ip = require('ip');
 
 // controller untuk register
-exports.registrasi = (req, res) => {
-    const post = {
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
-        role: req.body.role,
-        tanggal_daftar: new Date()
+exports.registrasi = function (req, res) {
+    var post = {
+         username: req.body.username,
+         email: req.body.email,
+         password: req.body.password,
+         role: req.body.role,
+         tanggal_daftar: new Date(),
     }
 
-    const query = "SELECT email FROM ?? WHERE ??"
-    const table = ["user", "email", post.email]
+    var query = "SELECT email FROM ?? WHERE ??=?";
+    var table = ["user", "email", post.email];
 
-    query = mysql.format(query,table)
+    query = mysql.format(query, table);
 
-    // dia akan mengecek apakah ada emailnya?, kalau belum terdaftar maka akan di tambahkan ke table user
-    connection.query(query, (error, rows) => {
-        if(error){
-            console.log(error);
-        }else{
+    // dia akan mengecek apakah emailnya sudah terdaftar atau belum
+    connection.query(query, function (error, rows) {
+          if (error) {
+               console.log(error);
+          } else {
             if(rows.length == 0){
-                const query = "INSERT INTO ?? SET ?"
-                const table = ["user"]
-                query = mysql.format(query, table)
-                connection.query(query, post, (error, rows)=> {
-                    if(error){
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
                         console.log(error);
-                    }else{
+                    } else {
                         response.ok("Berhasil menambahkan data user baru", res)
                     }
                 })
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // parse aplication/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // panggil routes
 var routes = require('./routes');
 routes(app);
 
+// daftarkan menu routes dari index
+// karena routesnya ada di middleware
+app.use('/auth', require('./middleware'));
+
+
  app.listen(2311, ()=> {
-    console.log('Server started on port');
+    console.log('Server started on port 2311');
  });
